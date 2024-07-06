9568033d9d90738b4e56c592b7a390de26cf6697
Harshit <harshit.14m@gmail.com>
2024-07-06 20:12:34 +0530

    string methods

--- JS/app.js
+++ JS/app.js
@@ -60,5 +60,66 @@
 // let str = "\u{1F60E}";
 // console.log(str);
 
-let str = "My name is Harshit";
-console.log(str.length);
+// let str = "My name is Harshit\n";
+// console.log(str);
+// console.log(str.length);
+// console.log(str.charAt(0));
+// console.log(str[5]);
+// console.log(str.charAt(20)); // empty string
+// console.log(str[25]); // undefined
+
+// let str = "Hello";
+// console.log(str[0]);
+// str[0] = "M";
+// console.log(str);
+// str = "M"+str[1]+str[2]+str[3]+str[4]
+// console.log(str);
+
+// let str = "For Hello World";
+// console.log(str.indexOf("hello"));
+// console.log(str.indexOf("z"));
+// console.log(str.indexOf("Hello"));
+// console.log(str.indexOf("o"));
+// console.log(str.indexOf("o", 2));
+// console.log(str.indexOf("o", 9));
+// console.log(str.indexOf("o", 12));
+
+// console.log(str.lastIndexOf("o")); //3 11
+// console.log(str.lastIndexOf("o", 9)); //6 8
+// console.log(str.lastIndexOf("o", 7)); //13 1
+// console.log(str.lastIndexOf("o", 0)); //-1 -1
+
+
+// let str = "For Hello World";
+// console.log(str.includes("o"));
+// console.log(str.includes("Hello"));
+// console.log(str.includes("hello"));
+// console.log(str.includes(" H"));
+// console.log(str.includes("o", 3));
+// console.log(str.includes("o", 12));
+
+// let str = "For Hello World";
+// console.log(str.startsWith("F"));
+// console.log(str.startsWith("f"));
+// console.log(str.startsWith("For"));
+// console.log(str.startsWith("for"));
+// console.log(str.startsWith("Hello"));
+// console.log(str.startsWith("For Hello"));
+
+// let str = "For Hello World";
+// console.log(str.endsWith("d"));
+// console.log(str.endsWith("ld"));
+// console.log(str.endsWith("D"));
+// console.log(str.endsWith("World"));
+// console.log(str.endsWith("world"));
+// console.log(str.endsWith("o"));
+// console.log(str.endsWith("Hello World"));
+// console.log(str.endsWith("Hello"));
+
+// let str = "paragraph";
+// console.log(str.slice(0,4));
+// console.log(str.slice(4));
+// console.log(str.slice(4, str.length));
+// console.log(str.slice(-4, -2)); //5 7
+// console.log(str.slice(-5, -1));
+// console.log(str.slice(-5));
